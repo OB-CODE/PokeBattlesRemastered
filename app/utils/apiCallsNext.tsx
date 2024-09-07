eb15fa9284929f5b72643744eb43ac4f5c7606ba
Mitchell O'Brien <mitchell.s.obrien@gmail.com>
2024-09-07 22:19:33 +1000

    adding local storage cache to getPokemon call as these base stats do not change

--- app/utils/apiCallsNext.tsx
+++ app/utils/apiCallsNext.tsx
@@ -18,15 +18,30 @@ const dynamodb = DynamoDBDocumentClient.from(client);
 export async function GetAllBasePokemonDetails() {
   const fetchData = async () => {
     try {
-      const response = await fetch("/api/getPokemon");
-      const data = await response.json();
-      pokemonDataStore.getState().setPokemonMainArr(data);
-      console.log(data);
+      // Check if data is already stored in session storage
+      const cachedData = sessionStorage.getItem("pokemonData");
+
+      if (cachedData) {
+        // Parse and use the cached data
+        const parsedData = JSON.parse(cachedData);
+        pokemonDataStore.getState().setPokemonMainArr(parsedData);
+        console.log("Using cached data:", parsedData);
+      } else {
+        // Make API call if no cached data is found
+        const response = await fetch("/api/getPokemon");
+        const data = await response.json();
+
+        // Store the fetched data in session storage
+        sessionStorage.setItem("pokemonData", JSON.stringify(data));
+
+        // Set the data in your Zustand store
+        pokemonDataStore.getState().setPokemonMainArr(data);
+        console.log("Fetched new data:", data);
+      }
     } catch (error) {
       console.error("Error fetching the data:", error);
     }
   };
 
   await fetchData();
-  //   return <div>GetAllBasePokemonDetails</div>;
 }
