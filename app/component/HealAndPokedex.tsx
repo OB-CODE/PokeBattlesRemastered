477f229fef261198b1b62052d919d03250c1f62a
Mitchell O'Brien <mitchell.s.obrien@gmail.com>
2025-03-01 23:57:50 +1100

    Added shop and backpack buttons with fixed css.

--- app/component/HealAndPokedex.tsx
+++ app/component/HealAndPokedex.tsx
@@ -4,6 +4,7 @@ import PokemonParty, { IPokemonMergedProps } from "./PokemonParty";
 import { constructionToast } from "../utils/helperfn";
 import { IallBattleStateInfo } from "../GameMainPage";
 import HealIndex from "./Heal/HealIndex";
+import { backpackSCG, shopSVG } from "../utils/UI/svgs";
 
 export interface IhealPokemonInfo {
   showHealPokemon: boolean;
@@ -24,16 +25,37 @@ const HealAndPokedex = (allBattleStateInfo: IallBattleStateInfo) => {
   return (
     <div className="flex flex-col w-full h-[calc(100%-60px)] items-center justify-start">
       <div className="flex justify-between w-[90%] mt-3">
-        <button
-          onClick={() => setShowHealPokemon(true)}
-          className="text-black bg-yellow-300 hover:bg-yellow-400 w-fit py-1 px-3 border-2 border-black rounded-xl">
-          Heal Pokemon
-        </button>
-        <button
-          className="text-black bg-yellow-300 hover:bg-yellow-400 w-fit py-1 px-3 border-2 border-black rounded-xl"
-          onClick={() => setShowPokedex(!showPokedex)}>
-          {showPokedex ? "POKEMON PARTY" : "POKEDEX"}
-        </button>
+        <div className="flex sm:flex-row gap-3 flex-col">
+          <button
+            onClick={() => setShowHealPokemon(true)}
+            className="text-black bg-yellow-300 hover:bg-yellow-400 w-fit py-1 px-3 border-2 border-black rounded-xl">
+            Heal Pokemon
+          </button>
+          <button
+            onClick={() => constructionToast()}
+            className="text-black bg-blue-300 hover:bg-blue-400 w-10  border-2 border-black rounded-xl">
+            <div className="flex justify-center items-center h-10">
+              {shopSVG}
+            </div>
+          </button>
+        </div>
+        <div className="flex sm:flex-row gap-3 flex-col-reverse justify-end">
+          <div className="flex justify-end">
+            <button
+              onClick={() => constructionToast()}
+              className="text-black bg-blue-300 hover:bg-blue-400 w-10  border-2 border-black rounded-xl">
+              <div className="flex w-full justify-center  items-center h-10">
+                {backpackSCG}
+              </div>
+            </button>
+          </div>
+
+          <button
+            className="text-black bg-yellow-300 hover:bg-yellow-400 w-fit py-1 px-3 border-2 border-black rounded-xl"
+            onClick={() => setShowPokedex(!showPokedex)}>
+            {showPokedex ? "POKEMON PARTY" : "POKEDEX"}
+          </button>
+        </div>
       </div>
       {showPokedex ? <Pokedex /> : <PokemonParty {...allBattleStateInfo} />}
 
