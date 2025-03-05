79f738de7ef452be817c7c0edd6aa73026ac2fad
Mitchell O'Brien <mitchell.s.obrien@gmail.com>
2025-03-05 22:06:58 +1100

    Added two more items to the store and arranged the store CSS for better display. Placeholders only.

--- app/component/shop/ShopBody.tsx
+++ app/component/shop/ShopBody.tsx
@@ -3,6 +3,18 @@ import React from "react";
 const ShopBody = () => {
   let shopItems = [
     {
+      name: "Pokeball",
+      cost: 10,
+      description: "A basic pokeball",
+      logo: "X",
+    },
+    {
+      name: "Golden Pokeball",
+      cost: 30,
+      description: "15% extra chance to catch a pokemon.",
+      logo: "X",
+    },
+    {
       name: "Small health potion",
       cost: 20,
       description: "Heals 20 health.",
@@ -17,24 +29,32 @@ const ShopBody = () => {
   ];
 
   return (
-    <div className="flex w-full justify-between   ">
+    <div className="flex w-full h-full justify-between flex-wrap">
       {shopItems.map((item) => {
         return (
-          <div className="flex flex-col w-[48%] bg-purple-100 rounded-xl p-1">
-            <div className="w-full flex justify-between py-1">
-              <div className="w-full flex justify-center pl-8">
-                <div className="border rounded-3xl w-10 h-10 flex justify-center items-center bg-green-100">
-                  {item.logo}
+          <div className="flex flex-col justify-between w-[46%] m-1 my-3 bg-purple-100 rounded-xl p-1">
+            <div>
+              <div className="w-full flex justify-between py-1">
+                <div className="w-full flex justify-center pl-8">
+                  <div className="border rounded-3xl w-10 h-10 flex justify-center items-center bg-green-100">
+                    {item.logo}
+                  </div>
+                </div>
+
+                <div className="border rounded-3xl w-8 h-8 flex justify-center items-center bg-yellow-400">
+                  ${item.cost}
                 </div>
               </div>
 
-              <div className="border rounded-3xl w-8 h-8 flex justify-center items-center bg-yellow-400">
-                ${item.cost}
-              </div>
+              <div className="font-bold">{item.name}</div>
+              <div className="italic">{item.description}</div>
             </div>
 
-            <div className="font-bold">{item.name}</div>
-            <div className="italic">{item.description}</div>
+            <div className="flex justify-center w-full">
+              <button className="text-black bg-yellow-300 hover:bg-yellow-400 w-fit py-1 px-3 border-2 border-black rounded-xl">
+                Buy
+              </button>
+            </div>
           </div>
         );
       })}
