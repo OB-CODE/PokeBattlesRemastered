786ea4a4aec53e3e44f4bf57b7303194f9ffc1ec
OBCODE <mitchell.s.obrien@gmail.com>
2025-06-10 22:51:35 +1000

    wrapping Auth0 for hydrations related client side.

---
+++ app/component/auth/AuthWrapper.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Auth0Provider } from "@auth0/auth0-react";
+import { useState, useEffect } from "react";
+import { getConfig } from "../../config";
+
+export default function AuthWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [redirectUri, setRedirectUri] = useState<string | null>(null);
+  const config = getConfig();
+
+  useEffect(() => {
+    setRedirectUri(window.location.origin);
+  }, []);
+
+  if (!redirectUri) {
+    return (
+      <div className="w-screen h-screen flex items-center justify-center">
+        Loading...
+      </div>
+    ); // Show loading instead of null
+  }
+
+  const providerConfig = {
+    domain: config.domain,
+    clientId: config.clientId,
+    authorizationParams: {
+      redirect_uri: redirectUri,
+      ...(config.authorizationParams?.audience
+        ? { audience: config.authorizationParams.audience }
+        : null),
+    },
+  };
+
+  return <Auth0Provider {...providerConfig}>{children}</Auth0Provider>;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,19 @@
 "use client";
-import { Auth0Provider } from "@auth0/auth0-react";
-import "./globals.css";
 import { Inter } from "next/font/google";
-import { useEffect, useState } from "react";
+import AuthWrapper from "./component/auth/AuthWrapper";
 import { getConfig } from "./config";
-
-const inter = Inter({ subsets: ["latin"] });
-
-const config = getConfig();
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [redirectUri, setRedirectUri] = useState<string | null>(null);
-
-  useEffect(() => {
-    setRedirectUri(window.location.origin);
-  }, []);
-
-  if (!redirectUri) return null; // or a loading spinner
-
-  const providerConfig = {
-    domain: config.domain,
-    clientId: config.clientId,
-    authorizationParams: {
-      redirect_uri: redirectUri,
-      ...(config.authorizationParams?.audience
-        ? { audience: config.authorizationParams.audience }
-        : null),
-    },
-  };
-
   return (
     <html lang="en">
-      <Auth0Provider {...providerConfig}>
-        <body className={inter.className}>{children}</body>
-      </Auth0Provider>
+      <body>
+        <AuthWrapper>{children}</AuthWrapper>
+      </body>
     </html>
   );
 }
