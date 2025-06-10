e048a4e57f7e409596bd38005bbcb4cfbc0a5077
OBCODE <mitchell.s.obrien@gmail.com>
2025-06-10 20:44:08 +1000

    Layout page adjusted for config update.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,40 @@
 "use client";
 import { Auth0Provider } from "@auth0/auth0-react";
 import "./globals.css";
-// import type { Metadata } from 'next'
 import { Inter } from "next/font/google";
-
+import { useEffect, useState } from "react";
 import { getConfig } from "./config";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// Please see https://auth0.github.io/auth0-react/interfaces/Auth0ProviderOptions.html
-// for a full list of the available properties on the provider
 const config = getConfig();
-
-const providerConfig = {
-  domain: config.domain,
-  clientId: config.clientId,
-  authorizationParams: {
-    redirect_uri: window.location.origin,
-    ...(config.authorizationParams?.audience
-      ? { audience: config.authorizationParams.audience }
-      : null),
-  },
-};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [redirectUri, setRedirectUri] = useState<string | null>(null);
+
+  useEffect(() => {
+    setRedirectUri(window.location.origin);
+  }, []);
+
+  if (!redirectUri) return null; // or a loading spinner
+
+  const providerConfig = {
+    domain: config.domain,
+    clientId: config.clientId,
+    authorizationParams: {
+      redirect_uri: redirectUri,
+      ...(config.authorizationParams?.audience
+        ? { audience: config.authorizationParams.audience }
+        : null),
+    },
+  };
+
   return (
     <html lang="en">
-      {/* Thanks a lot, In my case I was wrapping the body in the <Provider> of redux, by keeping <Provider> within body the issue has been resolved.
-      https://stackoverflow.com/questions/72509865/error-there-was-an-error-while-hydrating-because-the-error-happened-outside-of
-      */}
       <Auth0Provider {...providerConfig}>
         <body className={inter.className}>{children}</body>
       </Auth0Provider>
