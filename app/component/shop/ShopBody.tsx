de70228edaf2845114af81a13c76ebfd508b005f
OBCODE <mitchell.s.obrien@gmail.com>
2025-07-18 18:39:55 +1000

    Added the ability to sell items back to the store.

--- app/component/shop/ShopBody.tsx
+++ app/component/shop/ShopBody.tsx
@@ -11,16 +11,21 @@ interface IShopItem {
   description: string | Function;
   logo: string | StaticImageData;
   buySingleAction: () => void;
+  sellSingleAction: () => void;
   qty: number;
 }
 
 const ShopBody = () => {
   let moneyOwned = itemsStore((state) => state.moneyOwned);
   const decreaseMoneyOwned = itemsStore((state) => state.decreaseMoneyOwned);
+  const increaseMoneyOwned = itemsStore((state) => state.increaseMoneyOwned);
 
   const pokeballsOwned = itemsStore((state) => state.pokeballsOwned);
   const increasePokeballsOwned = itemsStore(
     (state) => state.increasePokeballsOwned
+  );
+  const decreasePokeballsOwned = itemsStore(
+    (state) => state.decreasePokeballsOwned
   );
 
   const goldenPokeballsOwned = itemsStore(
@@ -29,17 +34,28 @@ const ShopBody = () => {
   const increaseGoldenPokeballsOwned = itemsStore(
     (state) => state.increaseGoldenPokeballsOwned
   );
+  const decreaseGoldenPokeballsOwned = itemsStore(
+    (state) => state.decreaseGoldenPokeballsOwned
+  );
+
   const smallHealthPotionsOwned = itemsStore(
     (state) => state.smallHealthPotionsOwned
   );
   const increaseSmallHealthPotionsOwned = itemsStore(
     (state) => state.increaseSmallHealthPotionsOwned
   );
+  const decreaseSmallHealthPotionsOwned = itemsStore(
+    (state) => state.decreaseSmallHealthPotionsOwned
+  );
+
   const largeHealthPotionsOwned = itemsStore(
     (state) => state.largeHealthPotionsOwned
   );
   const increaseLargeHealthPotionsOwned = itemsStore(
     (state) => state.increaseLargeHealthPotionsOwned
+  );
+  const decreaseLargeHealthPotionsOwned = itemsStore(
+    (state) => state.decreaseLargeHealthPotionsOwned
   );
 
   let shopItems: IShopItem[] = [
@@ -51,6 +67,9 @@ const ShopBody = () => {
       buySingleAction: () => {
         increasePokeballsOwned(1);
       },
+      sellSingleAction: () => {
+        decreasePokeballsOwned(1);
+      },
       qty: pokeballsOwned,
     },
     {
@@ -60,6 +79,9 @@ const ShopBody = () => {
       logo: pokeballMapping.goldenPokeball.imgDes,
       buySingleAction: () => {
         increaseGoldenPokeballsOwned(1);
+      },
+      sellSingleAction: () => {
+        decreaseGoldenPokeballsOwned(1);
       },
       qty: goldenPokeballsOwned,
     },
@@ -71,6 +93,9 @@ const ShopBody = () => {
       buySingleAction: () => {
         increaseSmallHealthPotionsOwned(1);
       },
+      sellSingleAction: () => {
+        decreaseSmallHealthPotionsOwned(1);
+      },
       qty: smallHealthPotionsOwned,
     },
     {
@@ -80,6 +105,9 @@ const ShopBody = () => {
       logo: potionMapping.large.imgDes,
       buySingleAction: () => {
         increaseLargeHealthPotionsOwned(1);
+      },
+      sellSingleAction: () => {
+        decreaseLargeHealthPotionsOwned(1);
       },
       qty: largeHealthPotionsOwned,
     },
@@ -100,6 +128,20 @@ const ShopBody = () => {
     }
   }
 
+  function attemptSale(item: IShopItem) {
+    if (item.qty > 0) {
+      // Calculate sale price (half the cost, rounded to nearest whole number)
+      const salePrice = Math.round(item.cost / 2);
+      increaseMoneyOwned(salePrice);
+      item.sellSingleAction();
+      toast.success(
+        `You sold ${item.name} for $${salePrice}. You now have $${moneyOwned + salePrice} and ${item.qty - 1} ${item.name}(s) remaining.`
+      );
+    } else {
+      toast.warn(`You don't have any ${item.name} to sell.`);
+    }
+  }
+
   return (
     <div className="flex w-full h-full justify-between flex-wrap">
       <div className="w-full bold text-lg border bg-yellow-200">
@@ -116,7 +158,7 @@ const ShopBody = () => {
               <div className="flex mb-1S pl-1">Owned: {item.qty}</div>
               <div className="w-full flex justify-between py-1">
                 <div className="w-full flex justify-center pl-8">
-                  <div className="border rounded-3xl w-10 h-10 flex justify-center items-center bg-green-100">
+                  <div className="border rounded-3xl w-14 h-14 flex justify-center items-center bg-green-100">
                     {item.logo != "X" ? (
                       <Image
                         src={`/${item.logo}`}
@@ -129,9 +171,13 @@ const ShopBody = () => {
                     )}
                   </div>
                 </div>
-
-                <div className="border rounded-3xl w-8 h-8 flex justify-center items-center bg-yellow-400">
-                  ${item.cost}
+                <div className="flex flex-col justify-center items-center">
+                  <div className="border rounded-3xl w-8 h-8 flex justify-center items-center bg-yellow-400">
+                    ${item.cost}
+                  </div>
+                  <div className="border rounded-3xl w-8 h-8 flex justify-center items-center bg-red-300">
+                    ${item.cost / 2}
+                  </div>
                 </div>
               </div>
 
@@ -143,12 +189,18 @@ const ShopBody = () => {
               </div>
             </div>
 
-            <div className="flex justify-center w-full">
+            <div className="flex justify-between w-full px-2">
               <button
                 className="text-black bg-yellow-300 hover:bg-yellow-400 w-fit py-1 px-3 border-2 border-black rounded-xl"
                 onClick={() => attemptPurchase(item)}
               >
                 Buy
+              </button>
+              <button
+                className="text-black bg-red-300 hover:bg-red-400 w-fit py-1 px-3 border-2 border-black rounded-xl"
+                onClick={() => attemptSale(item)}
+              >
+                Sell
               </button>
             </div>
           </div>
